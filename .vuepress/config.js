7b6aa19dd75b47d59e8821264bcd4099785b5b2d
Abreto Fu <m@abreto.net>
2018-08-11 03:42:58 +0800

    add meta info

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,18 @@
 module.exports = {
+    title: 'AC-Voyage',
+    description: 'Make it accepted.',
+
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Library', link: '/library' },
+            { text: 'Templates', link: '/templates' },
+        ],
+        lastUpdated: 'Last Updated',
+        repo: 'ac-voyage/ac-voyage',
+        editLinks: true,
+    },
+
     head: [
         ['link', { rel: 'stylesheet', href: 'https://cdn.bootcss.com/KaTeX/0.6.0/katex.min.css' }],
     ],
